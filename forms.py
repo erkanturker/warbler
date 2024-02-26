0a186584991c26813150353271d42b250d1b1ccd
Erkan Turker <erkanturker@Erkans-MacBook-Pro.local>
2024-02-26 13:34:10 -0500

    adding update form and update user

--- forms.py
+++ forms.py
@@ -22,3 +22,18 @@ class LoginForm(FlaskForm):
 
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[Length(min=6)])
+
+class UserEditForm(FlaskForm):
+    password = PasswordField('Password', validators=[Length(min=6)])
+    username = StringField('Username',validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired()])
+    image_url = StringField('Image URL')
+    header_image_url = StringField('Header Image URL')
+    location = StringField('Location')
+    bio = StringField('Bio')
+    
+    
+
+
+
+
